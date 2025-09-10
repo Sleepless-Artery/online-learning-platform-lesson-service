052bf3c2733f92872b3d4dd068bdf986151b5b5e
ART3RY-G1N <cplusplus3414@gmail.com>
2025-09-11 02:11:45 +0300

    Updated Redis configuration

--- src/main/java/org/sleepless_artery/lesson_service/config/redis/properties/RedisConfigProperties.java
+++ src/main/java/org/sleepless_artery/lesson_service/config/redis/properties/RedisConfigProperties.java
@@ -1,7 +1,5 @@
 package org.sleepless_artery.lesson_service.config.redis.properties;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -15,9 +13,20 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class RedisConfigProperties {
 
-    @NotBlank(message = "Hostname must be defined")
     private String host;
-
-    @NotNull(message = "Port must be defined")
     private int port;
+    private String password;
+
+    private Cluster cluster = new Cluster();
+
+
+    @Getter @Setter
+    public static class Cluster {
+        private String nodes;
+        private Integer maxRedirects;
+    }
+
+    public boolean isClusterMode() {
+        return cluster != null && cluster.getNodes() != null && !cluster.getNodes().isEmpty();
+    }
 }
